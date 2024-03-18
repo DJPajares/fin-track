7c75937715bcbac3b1e90280e2a11d4698e98099
DJPajares <dj.pajares@gmail.com>
2024-03-19 00:06:27 +0800

    chore: set temporary routes

---
+++ packages/api/src/routes/index.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+// import items here
+
+const router = express.Router();
+
+// insert routes here
+
+export default router;
---
+++ packages/api/src/routes/items/logs.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const router = express.Router();
+
+// get (date, categories, etc)
+
+// get all
+
+// add
+
+// update
+
+// delete
+
+export default router;
