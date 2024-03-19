534ed3285231e9e7610a4b281aa1344215880ed1
Darwin Jason Pajares <xsgdp001@singlife.com>
2024-03-19 17:50:40 +0800

    chore: use mongoose models in index

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,15 +1,36 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import routes from './routes';
 
 dotenv.config();
 
 const app = express();
+
 const port = process.env.PORT;
+const databaseUrl =
+  process.env.MONGOOSE_DATABASE_URL || 'mongodb://127.0.0.1/fintrack';
 
-app.get('/', (req, res) => {
-  res.send('Express + TypeScript Server');
-});
+// app.get('/', (req, res) => {
+//   res.send('Express + TypeScript Server');
+// });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+// Middleware
+app.use(express.json());
+// app.use(routes);
+
+const start = async () => {
+  try {
+    // await mongoose.connect(databaseUrl);
+    await mongoose.connect(databaseUrl);
+
+    app.listen(port, () => {
+      console.log(`[server]: Server is running at http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+start();
--- packages/api/src/routes/index.ts
+++ packages/api/src/routes/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 // import items here
+import categories from './items/categories';
 
 const router = express.Router();
 
 // insert routes here
+router.use('/api/employees', categories);
 
 export default router;
