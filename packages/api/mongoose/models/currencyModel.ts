861dd56f3145ff622acd28c1187d726a925ff95b
Darwin Jason Pajares <xsgdp001@singlife.com>
2024-04-16 16:22:56 +0800

    chore: reinstall next js

--- packages/api/mongoose/models/currencyModel.ts
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from 'mongoose';
-
-const currencySchema = new Schema({
-  name: { type: String, required: true }
-});
-
-const CurrencyModel = mongoose.model('Currency', currencySchema);
-
-export { currencySchema, CurrencyModel };
--- packages/api/mongoose/models/typeModel.ts
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from 'mongoose';
-
-const typeSchema = new Schema({
-  name: { type: String, required: true }
-});
-
-const TypeModel = mongoose.model('Type', typeSchema);
-
-export { typeSchema, TypeModel };
