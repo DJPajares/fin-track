a627feaf4f333cfeb2bcbf64e393805e73e77929
DJPajares <dj.pajares@gmail.com>
2024-04-20 15:47:19 +0800

    chore: add cors

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import router from './routes';
 import errorHandler from './middleware/errorHandler';
 
@@ -11,7 +12,14 @@ const app = express();
 const port = process.env.PORT || 3001;
 const databaseUrl = process.env.DATABASE_URL || 'mongodb://127.0.0.1/database';
 
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+};
+
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(router);
 app.use(errorHandler);
