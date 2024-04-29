da875aa58dbff6a305e43ce2e96e29b2385d914f
Darwin Jason Pajares <xsgdp001@singlife.com>
2024-04-29 16:58:34 +0800

    feat: change home screen

--- packages/web/app/page.tsx
+++ packages/web/app/page.tsx
@@ -1,60 +1,22 @@
 'use client';
 
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
-const dashboardUrl = 'http://localhost:3001/api/v1/transactionPayments';
-
-const fetchDashboardCategories = async () => {
-  try {
-    const date = new Date();
-    const currency = 'SGD';
-
-    const { status, data } = await axios.post(dashboardUrl, {
-      date,
-      currency
-    });
-
-    if (status === 200) {
-      return data.data;
-    }
-  } catch (error) {
-    console.error('Fetch failed', error);
-  }
-};
-
-const openCard = () => {};
-
-const Dashboard = () => {
-  const [dashboardMainData, setDashboardMainData] = useState({});
-  const [dashboardCategories, setDashboardCategories] = useState([]);
-
-  useEffect(() => {
-    const fetchInitialData = async () => {
-      const { main, categories } = await fetchDashboardCategories();
-
-      setDashboardCategories(categories);
-      setDashboardMainData(main);
-    };
-
-    fetchInitialData();
-  }, []);
+const Home = () => {
+  const router = useRouter();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-row align-middle">
-        {dashboardCategories.map((category: any) => (
-          <div
-            key={category._id}
-            className="border-2 border-solid border-black rounded-lg p-8"
-            onClick={openCard}
-          >
-            {category.name}
-          </div>
-        ))}
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="p-10">Welcome</h1>
+
+      <button
+        className="border-2 border-solid rounded-lg p-2"
+        onClick={() => router.push('/dashboard')}
+      >
+        Go to Dashboard
+      </button>
     </main>
   );
 };
 
-export default Dashboard;
+export default Home;
