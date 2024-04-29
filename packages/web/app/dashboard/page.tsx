fe16d29880198d26255017a49885bccbc3e58ed3
Darwin Jason Pajares <xsgdp001@singlife.com>
2024-04-29 17:26:19 +0800

    feat: add modal for category transactions

--- packages/web/app/dashboard/page.tsx
+++ packages/web/app/dashboard/page.tsx
@@ -4,16 +4,51 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import transactionPaymentsData from '../../mockData/transactionPayments.json';
 
+type ModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  category: any;
+};
+
+const Modal = ({ isOpen, onClose, category }: ModalProps) => {
+  if (!isOpen) return null;
+
+  const { name, transactions } = category;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-75">
+      <div className="bg-black p-8 rounded-lg">
+        <div className="flex flex-row items-center align-middle justify-between mb-8">
+          <h2 className="text-xl font-bold">{name}</h2>
+          <button onClick={onClose}>X</button>
+        </div>
+        <div className="mb-8">
+          {transactions.map((transaction: any) => (
+            <div key={transaction._id}>
+              <div>{transaction.name}</div>
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col items-end">
+          <button
+            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+            onClick={onClose}
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const dashboardUrl = 'http://localhost:3001/api/v1/transactionPayments';
 
 const useMockedData = process.env.NEXT_PUBLIC_USE_MOCKED_DATA || false;
 
-console.log(useMockedData);
-
 const fetchDashboardCategories = async () => {
   try {
     if (useMockedData) {
-      console.log(transactionPaymentsData);
       return transactionPaymentsData;
     } else {
       const date = new Date();
@@ -33,35 +68,11 @@ const fetchDashboardCategories = async () => {
   }
 };
 
-const Modal = ({
-  isOpen,
-  onClose
-}: {
-  isOpen: boolean;
-  onClose: () => void;
-}) => {
-  if (!isOpen) return null;
-
-  return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-75">
-      <div className="bg-white p-8 rounded-lg">
-        <h2 className="text-xl font-bold mb-4">Modal Title</h2>
-        <p>Modal content goes here.</p>
-        <button
-          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-          onClick={onClose}
-        >
-          Close
-        </button>
-      </div>
-    </div>
-  );
-};
-
 const Dashboard = () => {
   const [dashboardMainData, setDashboardMainData] = useState({});
   const [dashboardCategories, setDashboardCategories] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [dashboardCategory, setDashboardCategory] = useState({});
 
   useEffect(() => {
     const fetchInitialData = async () => {
@@ -74,15 +85,16 @@ const Dashboard = () => {
     fetchInitialData();
   }, []);
 
+  const openModal = (category: any) => {
+    setIsModalOpen(true);
+    setDashboardCategory(category);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-row align-middle">
         {dashboardCategories.map((category: any) => (
-          <div
-            key={category._id}
-            className="flex flex-col"
-            onClick={() => setIsModalOpen(true)}
-          >
+          <div key={category._id} onClick={() => openModal(category)}>
             <div className="border-2 border-solid rounded-lg p-8 m-2">
               <div>{category.name}</div>
               <div>{category.totalAmount}</div>
@@ -90,7 +102,12 @@ const Dashboard = () => {
           </div>
         ))}
       </div>
-      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        category={dashboardCategory}
+      />
     </main>
   );
 };
