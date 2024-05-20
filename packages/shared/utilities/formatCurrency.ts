e93092ab1bb227b82a08443fce8b51c214e22e9e
DJPajares <dj.pajares@gmail.com>
2024-05-20 23:51:57 +0800

    feat: add decimal places props to format currency utility

--- packages/shared/utilities/formatCurrency.ts
+++ packages/shared/utilities/formatCurrency.ts
@@ -3,17 +3,21 @@ import currencyLocaleMapping from '../../../constants/currencyLocaleMapping';
 type FormatCurrencyProps = {
   value: number;
   currency: string;
+  decimal?: number;
 };
 
 export const formatCurrency = ({
   value,
-  currency = 'PHP'
+  currency = 'PHP',
+  decimal = 0
 }: FormatCurrencyProps) => {
   const locale =
     currencyLocaleMapping[currency as keyof typeof currencyLocaleMapping];
 
   return new Intl.NumberFormat(locale, {
     style: 'currency',
-    currency
+    currency,
+    minimumFractionDigits: 0,
+    maximumFractionDigits: decimal
   }).format(value);
 };
