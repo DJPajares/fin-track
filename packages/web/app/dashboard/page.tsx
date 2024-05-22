79f152b772d4da7c7d9757542331de9b75442d0a
DJPajares <dj.pajares@gmail.com>
2024-05-22 12:00:06 +0800

    feat: add side menu

--- packages/web/app/dashboard/page.tsx
+++ packages/web/app/dashboard/page.tsx
@@ -26,6 +26,16 @@ import {
   TransactionPaymentMainProps,
   TransactionPaymentCategoryProps
 } from '../../../../shared/types/transactionPaymentTypes';
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger
+} from '@/components/ui/sheet';
 
 const dashboardUrl = 'http://localhost:3001/api/v1/transactionPayments';
 
@@ -101,162 +111,172 @@ const Dashboard = () => {
   };
 
   return (
-    <div>
-      <div className="flex flex-row justify-end">
-        <Button variant="ghost" size="icon">
-          <MenuIcon className="h-8 w-8" />
-        </Button>
+    <main className="flex flex-col min-h-screen">
+      <div className="flex flex-row justify-end p-2">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <MenuIcon className="h-8 w-8" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              {/* <SheetTitle>Test</SheetTitle>
+              <SheetDescription>Description goes here</SheetDescription> */}
+              <p className="text-left text-lg font-semibold">Main Menu</p>
+            </SheetHeader>
+            <div className="flex flex-col justify-start py-6">
+              <p>Dashboard</p>
+              <p>Transactions</p>
+            </div>
+            {/* <SheetFooter>Powered by: wonderTech</SheetFooter> */}
+          </SheetContent>
+        </Sheet>
       </div>
-      <main className="flex min-h-screen flex-col justify-around">
-        <div className="p-10">
-          <div className="flex flex-col justify-around">
-            <div className="flex flex-row items-center justify-between">
-              <p className="text-xl font-medium">Balance</p>
-              <p className="text-xl font-medium">
-                {formatCurrency({
-                  value: dashboardMainData.balance,
-                  currency: dashboardMainData.currency
-                })}
-              </p>
+      <div className="flex flex-col justify-between p-10">
+        <div className="flex flex-col justify-around">
+          <div className="flex flex-row items-center justify-between">
+            <p className="text-xl font-medium">Balance</p>
+            <p className="text-xl font-medium">
+              {formatCurrency({
+                value: dashboardMainData.balance,
+                currency: dashboardMainData.currency
+              })}
+            </p>
+          </div>
+
+          <div className="flex flex-row items-center justify-between">
+            <p className="text-sm">Extra</p>
+            <p className="text-sm">
+              {formatCurrency({
+                value: dashboardMainData.extra,
+                currency: dashboardMainData.currency
+              })}
+            </p>
+          </div>
+
+          <div>
+            <div className="pt-8">
+              <Progress
+                value={
+                  (dashboardMainData.totalPaidAmount /
+                    dashboardMainData.totalAmount) *
+                  100
+                }
+              />
             </div>
 
-            <div className="flex flex-row items-center justify-between">
-              <p className="text-sm">Extra</p>
-              <p className="text-sm">
-                {formatCurrency({
-                  value: dashboardMainData.extra,
-                  currency: dashboardMainData.currency
-                })}
-              </p>
+            <div className="pt-4">
+              <p className="text font-medium text-center">Completed</p>
             </div>
 
             <div>
-              <div className="pt-8">
-                <Progress
-                  value={
-                    (dashboardMainData.totalPaidAmount /
-                      dashboardMainData.totalAmount) *
+              <p className="text-2xl font-bold text-center">
+                {Math.floor(
+                  (dashboardMainData.totalPaidAmount /
+                    dashboardMainData.totalAmount) *
                     100
-                  }
-                />
-              </div>
-
-              <div className="pt-4">
-                <p className="text font-medium text-center">Completed</p>
-              </div>
-
-              <div>
-                <p className="text-2xl font-bold text-center">
-                  {Math.floor(
-                    (dashboardMainData.totalPaidAmount /
-                      dashboardMainData.totalAmount) *
-                      100
-                  )}
-                  %
-                </p>
-              </div>
+                ) || 0}
+                %
+              </p>
             </div>
           </div>
-
-          <div className="grid grid-cols-2 sm:gap-1 lg:gap-6 items-start justify-center cursor-pointer">
-            {dashboardCategories.map((category: any) => (
-              <div className="p-5" key={category._id}>
-                <Card onClick={() => handleCardClick(category)}>
-                  <CardHeader>
-                    <CardDescription>{category.name}</CardDescription>
-                    <CardTitle>
-                      {formatCurrency({
-                        value: category.totalAmount,
-                        currency: dashboardMainData.currency
-                      })}
-                    </CardTitle>
-                  </CardHeader>
-                  <CardContent>
-                    <p className="text-xs text-gray-500">Settled</p>
-                    {/* <Separator /> */}
-                    <div className="flex flex-row items-center pt-2">
-                      <p className="text-xs pr-2">
-                        {Math.floor(
-                          (category.totalPaidAmount / category.totalAmount) *
-                            100
-                        )}
-                        %
-                      </p>
-                      <Progress
-                        value={
-                          (category.totalPaidAmount / category.totalAmount) *
-                          100
-                        }
-                        className="w-[60%] h-2"
-                      />
-                    </div>
-                  </CardContent>
-                </Card>
-              </div>
-            ))}
-
-            <Dialog open={openDialog} onOpenChange={handleCloseDialog}>
-              <DialogContent>
-                <DialogHeader>
-                  <DialogTitle>{dashboardCategory.name}</DialogTitle>
-                </DialogHeader>
-                <div className="flex flex-col">
-                  {Object.keys(dashboardCategory).length > 0 &&
-                    dashboardCategory.transactions.map((transaction: any) => (
-                      <div
-                        key={transaction._id}
-                        // className="grid grid-flow-col auto-cols-max grid-cols-5 gap-2 items-center justify-stretch"
-                        className="grid grid-cols-9 gap-2 items-center py-1"
-                      >
-                        <div className="col-span-2">{transaction.name}</div>
-                        <div className="col-span-2 text-right">
-                          <p className="text-sm">
-                            {formatCurrency({
-                              value: transaction.amount,
-                              currency: dashboardMainData.currency
-                            })}
-                          </p>
-                        </div>
-                        <div className="col-span-2">
-                          <Progress
-                            value={
-                              (transaction.paidAmount / transaction.amount) *
-                              100
-                            }
-                            className="h-3"
-                          />
-                        </div>
-                        <div className="text-left">
-                          <p className="text-xs">
-                            {Math.floor(
-                              (transaction.paidAmount / transaction.amount) *
-                                100
-                            )}
-                            %
-                          </p>
-                        </div>
-                        <div className="flex flex-row justify-end col-span-2">
-                          <Button variant="outline" size="icon">
-                            <HandCoinsIcon className="h-4 w-4" />
-                          </Button>
-                          <Button variant="outline" size="icon">
-                            <CheckIcon className="h-4 w-4" />
-                          </Button>
-                        </div>
-                      </div>
-                    ))}
-                </div>
-                <DialogFooter>
-                  <DialogClose>
-                    <Button>OK</Button>
-                  </DialogClose>
-                </DialogFooter>
-              </DialogContent>
-            </Dialog>
-          </div>
         </div>
-      </main>
-    </div>
+
+        <div className="grid grid-cols-2 sm:gap-1 lg:gap-6 items-start justify-center cursor-pointer">
+          {dashboardCategories.map((category: any) => (
+            <div className="p-5" key={category._id}>
+              <Card onClick={() => handleCardClick(category)}>
+                <CardHeader>
+                  <CardDescription>{category.name}</CardDescription>
+                  <CardTitle>
+                    {formatCurrency({
+                      value: category.totalAmount,
+                      currency: dashboardMainData.currency
+                    })}
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p className="text-xs text-gray-500">Settled</p>
+                  {/* <Separator /> */}
+                  <div className="flex flex-row items-center pt-2">
+                    <p className="text-xs pr-2">
+                      {Math.floor(
+                        (category.totalPaidAmount / category.totalAmount) * 100
+                      )}
+                      %
+                    </p>
+                    <Progress
+                      value={
+                        (category.totalPaidAmount / category.totalAmount) * 100
+                      }
+                      className="w-[60%] h-2"
+                    />
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
+          ))}
+
+          <Dialog open={openDialog} onOpenChange={handleCloseDialog}>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>{dashboardCategory.name}</DialogTitle>
+              </DialogHeader>
+              <div className="flex flex-col">
+                {Object.keys(dashboardCategory).length > 0 &&
+                  dashboardCategory.transactions.map((transaction: any) => (
+                    <div
+                      key={transaction._id}
+                      // className="grid grid-flow-col auto-cols-max grid-cols-5 gap-2 items-center justify-stretch"
+                      className="grid grid-cols-9 gap-2 items-center py-1"
+                    >
+                      <div className="col-span-2">{transaction.name}</div>
+                      <div className="col-span-2 text-right">
+                        <p className="text-sm">
+                          {formatCurrency({
+                            value: transaction.amount,
+                            currency: dashboardMainData.currency
+                          })}
+                        </p>
+                      </div>
+                      <div className="col-span-2">
+                        <Progress
+                          value={
+                            (transaction.paidAmount / transaction.amount) * 100
+                          }
+                          className="h-3"
+                        />
+                      </div>
+                      <div className="text-left">
+                        <p className="text-xs">
+                          {Math.floor(
+                            (transaction.paidAmount / transaction.amount) * 100
+                          )}
+                          %
+                        </p>
+                      </div>
+                      <div className="flex flex-row justify-end col-span-2">
+                        <Button variant="outline" size="icon">
+                          <HandCoinsIcon className="h-4 w-4" />
+                        </Button>
+                        <Button variant="outline" size="icon">
+                          <CheckIcon className="h-4 w-4" />
+                        </Button>
+                      </div>
+                    </div>
+                  ))}
+              </div>
+              <DialogFooter>
+                <DialogClose>
+                  <Button>OK</Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
+      </div>
+    </main>
   );
 };
 
