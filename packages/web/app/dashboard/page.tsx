d196cd4a740e51eb0c49f3089357a739b420a998
DJPajares <dj.pajares@gmail.com>
2024-05-25 11:24:52 +0800

    feat: add skeleton

--- packages/web/app/dashboard/page.tsx
+++ packages/web/app/dashboard/page.tsx
@@ -45,29 +45,11 @@ import type {
   TransactionPaymentMainProps,
   TransactionPaymentCategoryProps
 } from '../../../../shared/types/transactionPaymentTypes';
+import { Skeleton } from '@/components/ui/skeleton';
 
 const dashboardUrl = 'http://localhost:3001/api/v1/transactionPayments';
 
 const useMockedData = process.env.NEXT_PUBLIC_USE_MOCKED_DATA === 'true';
-
-const initialMainData = {
-  currency: 'PHP',
-  budget: 0,
-  totalAmount: 0,
-  totalPaidAmount: 0,
-  balance: 0,
-  extra: 0,
-  paymentCompletionRate: 0
-};
-
-const initialCategoryData = {
-  _id: '',
-  name: '',
-  totalAmount: 0,
-  totalPaidAmount: 0,
-  paymentCompletionRate: 0,
-  transactions: []
-};
 
 const currencies = [
   {
@@ -122,10 +104,10 @@ const fetchTransactionPayments = async (date: Date, currency: string) => {
 
 const Dashboard = () => {
   const [dashboardMainData, setDashboardMainData] =
-    useState<TransactionPaymentMainProps>(initialMainData);
+    useState<TransactionPaymentMainProps>({});
   const [dashboardCategories, setDashboardCategories] = useState([]);
   const [dashboardCategory, setDashboardCategory] =
-    useState<TransactionPaymentCategoryProps>(initialCategoryData);
+    useState<TransactionPaymentCategoryProps>({});
   const [openDialog, setOpenDialog] = useState(false);
   const [date, setDate] = useState(new Date());
   const [openDatePopover, setOpenDatePopover] = useState(false);
@@ -147,7 +129,7 @@ const Dashboard = () => {
     setDashboardMainData(main);
   };
 
-  const changeDate = async (newDate) => {
+  const changeDate = async (newDate: any) => {
     setDate(newDate);
     setOpenDatePopover(false);
   };
@@ -211,102 +193,138 @@ const Dashboard = () => {
 
       {/* CONTENT */}
       <div className="flex flex-col justify-between p-10">
-        <div className="flex flex-row items-center justify-between">
-          <Popover open={openDatePopover} onOpenChange={setOpenDatePopover}>
-            <PopoverTrigger asChild>
-              <Button variant="ghost" className="px-0">
-                <p className="text-2xl font-bold pr-2">{formatDate(date)}</p>
-                {/* <ChevronDownIcon className="h-4 w-4" /> */}
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0" align="start">
-              <Calendar
-                mode="single"
-                selected={date}
-                onSelect={changeDate}
-                initialFocus
-              />
-            </PopoverContent>
-          </Popover>
+        <div>
+          <div className="py-2">
+            {Object.keys(dashboardMainData).length > 0 ? (
+              <div className="flex flex-row items-center justify-between">
+                <Popover
+                  open={openDatePopover}
+                  onOpenChange={setOpenDatePopover}
+                >
+                  <PopoverTrigger asChild>
+                    <Button variant="ghost" className="px-0">
+                      <p className="text-3xl font-extrabold pr-2">
+                        {formatDate(date)}
+                      </p>
+                      {/* <ChevronDownIcon className="h-4 w-4" /> */}
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={date}
+                      onSelect={changeDate}
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
 
-          <Popover
-            open={openCurrencyPopover}
-            onOpenChange={setOpenCurrencyPopover}
-          >
-            <PopoverTrigger asChild>
-              <Button variant="ghost" className="px-0">
-                <p className="text-2xl font-bold">{currency}</p>
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-[200px] p-0">
-              <Command>
-                <CommandInput
-                  placeholder="Search currency..."
-                  className="h-9"
-                />
-                <CommandEmpty>No currencies found</CommandEmpty>
-                <CommandGroup>
-                  <CommandList>
-                    {currencies.map((currency) => (
-                      <CommandItem
-                        key={currency.id}
-                        value={currency.name}
-                        onSelect={(currentValue: any) => {
-                          setCurrency(
-                            currentValue === currency ? '' : currentValue
-                          );
-                          setOpenCurrencyPopover(false);
-                        }}
-                      >
-                        {currency.name}
-                      </CommandItem>
-                    ))}
-                  </CommandList>
-                </CommandGroup>
-              </Command>
-            </PopoverContent>
-          </Popover>
-        </div>
-
-        <div className="flex flex-col justify-around">
-          <div className="flex flex-row items-center justify-between">
-            <p className="text-xl font-medium">Balance</p>
-            <p className="text-xl font-medium">
-              {formatCurrency({
-                value: dashboardMainData.balance,
-                currency: dashboardMainData.currency
-              })}
-            </p>
+                <Popover
+                  open={openCurrencyPopover}
+                  onOpenChange={setOpenCurrencyPopover}
+                >
+                  <PopoverTrigger asChild>
+                    <Button variant="ghost" className="px-0">
+                      <p className="text-3xl font-extrabold">{currency}</p>
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-[200px] p-0">
+                    <Command>
+                      <CommandInput
+                        placeholder="Search currency..."
+                        className="h-9"
+                      />
+                      <CommandEmpty>No currencies found</CommandEmpty>
+                      <CommandGroup>
+                        <CommandList>
+                          {currencies.map((currency) => (
+                            <CommandItem
+                              key={currency.id}
+                              value={currency.name}
+                              onSelect={(currentValue: any) => {
+                                setCurrency(
+                                  currentValue === currency ? '' : currentValue
+                                );
+                                setOpenCurrencyPopover(false);
+                              }}
+                            >
+                              {currency.name}
+                            </CommandItem>
+                          ))}
+                        </CommandList>
+                      </CommandGroup>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
+              </div>
+            ) : (
+              <div className="flex flex-row items-center justify-between">
+                <Skeleton className="h-6 w-20" />
+                <Skeleton className="h-6 w-20" />
+              </div>
+            )}
           </div>
 
-          <div className="flex flex-row items-center justify-between">
-            <p className="text-sm">Extra</p>
-            <p className="text-sm">
-              {formatCurrency({
-                value: dashboardMainData.extra,
-                currency: dashboardMainData.currency
-              })}
-            </p>
+          <div className="py-1">
+            {Object.keys(dashboardMainData).length > 0 ? (
+              <div className="flex flex-row items-center justify-between">
+                <p className="text-xl font-medium">Balance</p>
+                <p className="text-xl font-medium">
+                  {formatCurrency({
+                    value: dashboardMainData.balance,
+                    currency: dashboardMainData.currency
+                  })}
+                </p>
+              </div>
+            ) : (
+              <div className="flex flex-row items-center justify-between">
+                <Skeleton className="h-6 w-20" />
+                <Skeleton className="h-6 w-20" />
+              </div>
+            )}
+          </div>
+
+          <div className="py-1">
+            {Object.keys(dashboardMainData).length > 0 ? (
+              <div className="flex flex-row items-center justify-between">
+                <p className="text-sm">Extra</p>
+                <p className="text-sm">
+                  {formatCurrency({
+                    value: dashboardMainData.extra,
+                    currency: dashboardMainData.currency
+                  })}
+                </p>
+              </div>
+            ) : (
+              <div className="flex flex-row items-center justify-between">
+                <Skeleton className="h-4 w-20" />
+                <Skeleton className="h-4 w-20" />
+              </div>
+            )}
           </div>
 
           <div className="flex flex-col items-center">
-            <CircularProgress
-              classNames={{
-                svg: 'w-36 h-36 drop-shadow-md',
-                // indicator: 'stroke-white',
-                track: 'stroke-white/10',
-                value: 'text-3xl font-semibold'
-              }}
-              label="Completed"
-              value={
-                (dashboardMainData.totalPaidAmount /
-                  dashboardMainData.totalAmount) *
-                100
-              }
-              strokeWidth={3}
-              color="success"
-              showValueLabel={true}
-            />
+            {Object.keys(dashboardMainData).length > 0 ? (
+              <CircularProgress
+                classNames={{
+                  svg: 'w-36 h-36 drop-shadow-md',
+                  // indicator: 'stroke-white',
+                  track: 'stroke-white/10',
+                  value: 'text-3xl font-semibold'
+                }}
+                label="Completed"
+                value={
+                  (dashboardMainData.totalPaidAmount /
+                    dashboardMainData.totalAmount) *
+                  100
+                }
+                strokeWidth={3}
+                color="success"
+                showValueLabel={true}
+              />
+            ) : (
+              <Skeleton className="h-36 w-36 rounded-full" />
+            )}
           </div>
         </div>
 
