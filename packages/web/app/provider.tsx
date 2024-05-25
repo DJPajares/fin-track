f591a1da098f8313aa7626efec67dc8d42e10cb6
DJPajares <dj.pajares@gmail.com>
2024-05-25 15:47:32 +0800

    feat: default theme value

--- packages/web/app/provider.tsx
+++ packages/web/app/provider.tsx
@@ -6,7 +6,9 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes';
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      <NextThemesProvider attribute="class">{children}</NextThemesProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        {children}
+      </NextThemesProvider>
     </NextUIProvider>
   );
 }
