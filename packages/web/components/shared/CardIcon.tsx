513324b101a0b91fbd370f6cf8a47aa228a0b209
Darwin Jason Pajares <xsgdp001@singlife.com>
2024-10-16 09:29:32 +0800

    refactor: use switch case in card icon

--- packages/web/components/shared/CardIcon.tsx
+++ packages/web/components/shared/CardIcon.tsx
@@ -7,27 +7,35 @@ import {
   UsersIcon
 } from 'lucide-react';
 
-type CardIconProps = {
-  icon: string;
+export type CardIconProps = {
+  icon:
+    | 'activity'
+    | 'users'
+    | 'credit-card'
+    | 'lightbulb'
+    | 'clapperboard'
+    | 'bed-double';
 };
 
 const CardIcon = ({ icon }: CardIconProps) => {
   const iconClassName = 'h-4 w-4 text-slate-500 dark:text-slate-400';
 
-  if (icon === 'activity') {
-    return <ActivityIcon className={iconClassName} />;
-  } else if (icon === 'users') {
-    return <UsersIcon className={iconClassName} />;
-  } else if (icon === 'credit-card') {
-    return <CreditCardIcon className={iconClassName} />;
-  } else if (icon === 'lightbulb') {
-    return <ActivityIcon className={iconClassName} />;
-  } else if (icon === 'clapperboard') {
-    return <ClapperboardIcon className={iconClassName} />;
-  } else if (icon === 'bed-double') {
-    return <BedDoubleIcon className={iconClassName} />;
-  } else {
-    return <DollarSignIcon className={iconClassName} />;
+  switch (icon) {
+    case 'activity':
+      return <ActivityIcon className={iconClassName} />;
+    case 'users':
+      return <UsersIcon className={iconClassName} />;
+    case 'credit-card':
+      return <CreditCardIcon className={iconClassName} />;
+    case 'lightbulb':
+      return <ActivityIcon className={iconClassName} />;
+    case 'clapperboard':
+      return <ClapperboardIcon className={iconClassName} />;
+    case 'bed-double':
+      return <BedDoubleIcon className={iconClassName} />;
+
+    default:
+      return <DollarSignIcon className={iconClassName} />;
   }
 };
 
