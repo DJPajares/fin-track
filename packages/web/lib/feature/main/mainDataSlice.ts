6918104f11018f34f08ceb8f245031476404c520
Darwin Jason Pajares <xsgdp001@singlife.com>
2024-10-23 15:49:31 +0800

    refactor: add type props for main data slice

--- packages/web/lib/feature/main/mainDataSlice.ts
+++ packages/web/lib/feature/main/mainDataSlice.ts
@@ -1,12 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import type { CategoryItemProps } from '@/types/Category';
 import type { ListProps } from '@/types/List';
 
+type CategoryProps = {
+  [key: string]: CategoryItemProps[];
+};
+
+type UpdateCategoryProps = {
+  type: ListProps;
+  category: CategoryItemProps;
+};
+
 type MainDataProps = {
   types: ListProps[];
-  categories: {
-    [key: string]: CategoryItemProps[];
-  };
+  categories: CategoryProps;
 };
 
 const initialState: MainDataProps = {
@@ -21,13 +28,13 @@ const categorySlice = createSlice({
   name: 'main',
   initialState,
   reducers: {
-    setTypes: (state, action) => {
+    setTypes: (state, action: PayloadAction<ListProps[]>) => {
       state.types = action.payload;
     },
-    setCategories: (state, action) => {
+    setCategories: (state, action: PayloadAction<CategoryProps>) => {
       state.categories = action.payload;
     },
-    updateCategory: (state, action) => {
+    updateCategory: (state, action: PayloadAction<UpdateCategoryProps>) => {
       const { type, category } = action.payload;
 
       const categories = state.categories[type._id];
