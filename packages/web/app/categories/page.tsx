a8cbadf0dc0416b22a9930a4f70310a69f6a0ec5
Darwin Jason Pajares <xsgdp001@singlife.com>
2024-10-23 14:57:39 +0800

    feat: update one category and list of categories in the ui

--- packages/web/app/categories/page.tsx
+++ packages/web/app/categories/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { useEffect, useRef, useState } from 'react';
-import { useAppDispatch, useAppSelector } from '@/lib/hooks';
+import { useAppSelector } from '@/lib/hooks';
 
 import { Separator } from '@/components/ui/separator';
 import { Button } from '@/components/ui/button';
@@ -12,202 +11,89 @@ import EditCategoryDrawer from '@/components/categories/EditCategoryDrawer';
 import { PlusIcon } from 'lucide-react';
 
 import type { CategoryItemProps } from '@/types/Category';
-import type { ListProps } from '@/types/List';
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger
-} from '@/components/ui/drawer';
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger
-} from '@/components/ui/alert-dialog';
-import { updateCategory } from '@/lib/feature/main/mainDataSlice';
 
 const Categories = () => {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  const [openDrawerId, setOpenDrawerId] = useState<string | null>(null); // Track which category's drawer is open
-
   const { types, categories } = useAppSelector((state) => state.main);
-
-  const formRef = useRef<HTMLFormElement>(null);
-
-  useEffect(() => {
-    console.log('categories', categories);
-  }, [categories]);
-
-  // const handleEditCategory = (type: ListProps, category: CategoryItemProps) => {
-  //   setCategoryItemType(type);
-  //   setCategoryItem(category);
-  //   // setIsDrawerOpen(true);
-  // };
-
-  const handleEditCategory = (type: ListProps, category: CategoryItemProps) => {
-    // Set the currently opened drawer's id
-    // setOpenDrawerId(category._id);
-  };
-
-  // const closeDrawer = () => {
-  //   setOpenDrawerId(null); // Close the drawer
-  // };
 
   const handleAddNewCategory = () => {
     // Open the add/edit category
     // setIsDrawerOpen(true);
   };
 
-  const handleAddSuggestion = (category: CategoryItemProps) => {
+  const handleAddSuggestedCategory = (category: CategoryItemProps) => {
     console.log(category);
   };
 
-  const handleSubmitButton = () => {
-    if (formRef.current) formRef.current.requestSubmit();
-
-    // console.log('categoryItem', categoryItem);
-    // dispatch(updateCategory(categoryItem));
-    // closeDrawer();
-    // setIsDrawerOpen(false);
-  };
-
   return (
-    <>
-      <div className="space-y-4 sm:space-y-8">
-        <Tabs
-          variant="bordered"
-          radius="full"
-          color="secondary"
-          className="flex flex-col items-center"
-        >
-          {types.map((type) => (
-            <Tab key={type._id.toString()} title={type.name}>
-              <div className="space-y-4">
-                <div className="flex flex-row items-center justify-between">
-                  <h6 className="font-semibold text-lg">CATEGORIES</h6>
+    <div className="space-y-4 sm:space-y-8">
+      <Tabs
+        variant="bordered"
+        radius="full"
+        color="secondary"
+        className="flex flex-col items-center"
+      >
+        {types.map((type) => (
+          <Tab key={type._id.toString()} title={type.name}>
+            <div className="space-y-4">
+              <div className="flex flex-row items-center justify-between">
+                <h6 className="font-semibold text-lg">CATEGORIES</h6>
 
-                  <Button
-                    variant="ghost"
-                    size="sm_rounded_icon"
-                    onClick={handleAddNewCategory}
-                  >
-                    <PlusIcon className="h-4 w-4" />
-                  </Button>
-                </div>
+                <Button
+                  variant="ghost"
+                  size="sm_rounded_icon"
+                  onClick={handleAddNewCategory}
+                >
+                  <PlusIcon className="h-4 w-4" />
+                </Button>
+              </div>
 
-                <div className="bg-secondary rounded-lg">
-                  {categories[type._id]
-                    .filter((category) => category.active)
-                    .map((category, i, { length }) => (
-                      <div key={category._id}>
-                        <Drawer>
-                          <DrawerTrigger asChild>
-                            <div
-                              className="flex flex-row items-center space-x-4 p-2 hover:bg-border cursor-pointer"
-                              // onClick={() => handleEditCategory(type, category)}
-                            >
-                              <CardIcon icon={category.icon} />
-                              <p>{category.name}</p>
-                            </div>
-                          </DrawerTrigger>
+              <div className="bg-secondary rounded-lg">
+                {categories[type._id]
+                  .filter((category) => category.active)
+                  .map((category, i, { length }) => (
+                    <div key={category._id}>
+                      <EditCategoryDrawer type={type} category={category}>
+                        <div className="flex flex-row items-center space-x-4 p-2 hover:bg-border cursor-pointer">
+                          <CardIcon icon={category.icon} />
+                          <p>{category.name}</p>
+                        </div>
+                      </EditCategoryDrawer>
 
-                          <DrawerContent className="mx-auto w-full max-w-lg overflow-y-scroll max-h-screen">
-                            <DrawerHeader>
-                              <DrawerTitle>Edit Category</DrawerTitle>
-                              <DrawerDescription>
-                                {category.name}
-                              </DrawerDescription>
-                            </DrawerHeader>
+                      {i + 1 !== length && <Separator />}
+                    </div>
+                  ))}
+              </div>
 
-                            <EditCategoryDrawer
-                              type={type}
-                              category={category}
-                              formRef={formRef}
-                            />
+              <h6 className="font-semibold text-lg">SUGGESTIONS</h6>
 
-                            <DrawerFooter>
-                              <AlertDialog>
-                                <AlertDialogTrigger asChild>
-                                  <Button>Ok</Button>
-                                </AlertDialogTrigger>
-                                <AlertDialogContent>
-                                  <AlertDialogHeader>
-                                    <AlertDialogTitle>
-                                      Are you sure?
-                                    </AlertDialogTitle>
-                                  </AlertDialogHeader>
-                                  <AlertDialogFooter>
-                                    <AlertDialogCancel>
-                                      Cancel
-                                    </AlertDialogCancel>
-                                    <AlertDialogAction
-                                      onClick={handleSubmitButton}
-                                    >
-                                      Ok
-                                    </AlertDialogAction>
-                                  </AlertDialogFooter>
-                                </AlertDialogContent>
-                              </AlertDialog>
-                              <DrawerClose asChild>
-                                <Button variant="outline">Cancel</Button>
-                              </DrawerClose>
-                            </DrawerFooter>
-                          </DrawerContent>
-                        </Drawer>
-
-                        {i + 1 !== length && <Separator />}
-                      </div>
-                    ))}
-                </div>
-
-                <h6 className="font-semibold text-lg">SUGGESTIONS</h6>
-
-                <div className="bg-secondary rounded-lg">
-                  {categories[type._id]
-                    .filter((category) => !category.active)
-                    .map((category, i, { length }) => (
-                      <div key={category._id}>
-                        <div
-                          className="flex flex-row items-center justify-between p-2"
-                          onClick={() => handleAddSuggestion(category)}
-                        >
-                          <div className="flex flex-row items-center space-x-4">
-                            <CardIcon icon={category.icon} />
-                            <p>{category.name}</p>
-                          </div>
-
-                          <Button variant="ghost" size="sm_rounded_icon">
-                            <PlusIcon className="h-4 w-4" />
-                          </Button>
+              <div className="bg-secondary rounded-lg">
+                {categories[type._id]
+                  .filter((category) => !category.active)
+                  .map((category, i, { length }) => (
+                    <div key={category._id}>
+                      <div
+                        className="flex flex-row items-center justify-between p-2"
+                        onClick={() => handleAddSuggestedCategory(category)}
+                      >
+                        <div className="flex flex-row items-center space-x-4">
+                          <CardIcon icon={category.icon} />
+                          <p>{category.name}</p>
                         </div>
 
-                        {i + 1 !== length && <Separator />}
+                        <Button variant="ghost" size="sm_rounded_icon">
+                          <PlusIcon className="h-4 w-4" />
+                        </Button>
                       </div>
-                    ))}
-                </div>
-              </div>
-            </Tab>
-          ))}
-        </Tabs>
-      </div>
 
-      {/* <EditCategoryDrawer
-        isDrawerOpen={isDrawerOpen}
-        setIsDrawerOpen={setIsDrawerOpen}
-        type={categoryItemType}
-        category={categoryItem}
-        setCategory={setCategoryItem}
-      /> */}
-    </>
+                      {i + 1 !== length && <Separator />}
+                    </div>
+                  ))}
+              </div>
+            </div>
+          </Tab>
+        ))}
+      </Tabs>
+    </div>
   );
 };
 
--- packages/web/components/categories/EditCategoryDrawer.tsx
+++ packages/web/components/categories/EditCategoryDrawer.tsx
@@ -1,60 +1,66 @@
-import {
-  Dispatch,
-  RefObject,
-  SetStateAction,
-  useEffect,
-  useState
-} from 'react';
+import { ReactNode, useRef, useState } from 'react';
 import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger
+} from '../ui/drawer';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger
+} from '../ui/alert-dialog';
 import { Input } from '../ui/input';
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
 import { Button } from '../ui/button';
+
+import { useAppDispatch } from '@/lib/hooks';
+import { updateCategory } from '@/lib/feature/main/mainDataSlice';
 
 import CardIcon, { iconMap, type IconProps } from '../shared/CardIcon';
 
 import type { ListProps } from '@/types/List';
 import type { CategoryItemProps } from '@/types/Category';
-import { useAppDispatch } from '@/lib/hooks';
-import { updateCategory } from '@/lib/feature/main/mainDataSlice';
 
 type EditCategoryDrawerProps = {
-  // isDrawerOpen: boolean;
-  // setIsDrawerOpen: Dispatch<SetStateAction<boolean>>;
   type?: ListProps;
   category: CategoryItemProps;
-  // setCategory: Dispatch<SetStateAction<CategoryItemProps>>;
-  formRef: RefObject<HTMLFormElement>;
+  children: ReactNode;
 };
 
 const iconMapArray = Object.keys(iconMap).map((key) => key);
 
 const EditCategoryDrawer = ({
-  // isDrawerOpen,
-  // setIsDrawerOpen,
   type,
   category,
-  // setCategory,
-  formRef
+  children
 }: EditCategoryDrawerProps) => {
   const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    form.setValue('icon', category.icon);
-    form.setValue('name', category.name);
-  }, [category]);
+  const formRef = useRef<HTMLFormElement>(null);
+
+  // useEffect(() => {
+  //   form.setValue('icon', category.icon);
+  //   form.setValue('name', category.name);
+  // }, [category]);
 
   const categorySchema = z.custom<CategoryItemProps>();
-  // const categorySchema = z.object({
-  //   _id: z.string(),
-  //   name: z.string(),
-  //   icon: 'activity' | 'users',
-  //   active: z.string()
-  // });
 
   const form = useForm<CategoryItemProps>({
     resolver: zodResolver(categorySchema),
@@ -67,72 +73,107 @@ const EditCategoryDrawer = ({
     setIsPopoverOpen(false);
   };
 
-  const onSubmit: SubmitHandler<CategoryItemProps> = (data) => {
-    // setCategory(data);
+  const handleConfirmSubmit = () => {
+    if (formRef.current) formRef.current.requestSubmit();
 
+    setIsDrawerOpen(false);
+  };
+
+  const onSubmit: SubmitHandler<CategoryItemProps> = (data) => {
     const result = {
       type,
       category: data
     };
 
-    console.log('result', result);
-
     dispatch(updateCategory(result));
   };
 
   return (
-    <form
-      onSubmit={form.handleSubmit(onSubmit)}
-      className="flex flex-row items-center p-4 space-x-2 sm:space-x-4"
-      ref={formRef}
-    >
-      <Controller
-        name="icon"
-        control={form.control}
-        render={({ field }) => (
-          <Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
-            <PopoverTrigger asChild>
-              <Button variant="outline" size="icon">
-                {/* <CardIcon icon={category.icon} /> */}
-                <CardIcon icon={field.value} />
-              </Button>
-            </PopoverTrigger>
+    <Drawer open={isDrawerOpen} onOpenChange={setIsDrawerOpen}>
+      <DrawerTrigger asChild>{children}</DrawerTrigger>
 
-            <PopoverContent className="w-full">
-              <div className="grid grid-cols-6 align-middle gap-2">
-                {iconMapArray.map((icon) => (
-                  <Button
-                    key={icon}
-                    variant="outline"
-                    size="icon"
-                    className={`${
-                      field.value === icon &&
-                      'bg-primary text-primary-foreground'
-                    }`}
-                    onClick={() => handleChangeIcon(icon)}
-                  >
-                    <CardIcon icon={icon} />
+      <DrawerContent className="mx-auto w-full max-w-lg overflow-y-scroll max-h-screen">
+        <DrawerHeader>
+          <DrawerTitle>Edit Category</DrawerTitle>
+          <DrawerDescription>{category.name}</DrawerDescription>
+        </DrawerHeader>
+
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-row items-center p-4 space-x-2 sm:space-x-4"
+          ref={formRef}
+        >
+          <Controller
+            name="icon"
+            control={form.control}
+            render={({ field }) => (
+              <Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
+                <PopoverTrigger asChild>
+                  <Button variant="outline" size="icon">
+                    {/* <CardIcon icon={category.icon} /> */}
+                    <CardIcon icon={field.value} />
                   </Button>
-                ))}
-              </div>
-            </PopoverContent>
-          </Popover>
-        )}
-      />
+                </PopoverTrigger>
 
-      <Controller
-        name="name"
-        control={form.control}
-        render={({ field }) => (
-          <Input
-            placeholder="Category name"
-            // value={field.value}
-            // onChange={field.onChange}
-            {...field}
+                <PopoverContent className="w-full">
+                  <div className="grid grid-cols-6 align-middle gap-2">
+                    {iconMapArray.map((icon) => (
+                      <Button
+                        key={icon}
+                        variant="outline"
+                        size="icon"
+                        className={`${
+                          field.value === icon &&
+                          'bg-primary text-primary-foreground'
+                        }`}
+                        onClick={() => handleChangeIcon(icon)}
+                      >
+                        <CardIcon icon={icon} />
+                      </Button>
+                    ))}
+                  </div>
+                </PopoverContent>
+              </Popover>
+            )}
           />
-        )}
-      />
-    </form>
+
+          <Controller
+            name="name"
+            control={form.control}
+            render={({ field }) => (
+              <Input
+                placeholder="Category name"
+                // value={field.value}
+                // onChange={field.onChange}
+                {...field}
+              />
+            )}
+          />
+        </form>
+
+        <DrawerFooter>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button>Ok</Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={handleConfirmSubmit}>
+                  Ok
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+          <DrawerClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   );
 };
 
