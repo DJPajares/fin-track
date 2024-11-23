cfe7f6e9ddaf3db6c8981492ad521f86260064e2
DJPajares <dj.pajares@gmail.com>
2024-11-23 15:44:11 +0800

    feat: use translation for category card

--- packages/web/app/dashboard/Category/CategoryCard.tsx
+++ packages/web/app/dashboard/Category/CategoryCard.tsx
@@ -5,6 +5,7 @@ import CardIcon from '@/components/shared/CardIcon';
 import { formatCurrency } from '../../../../../shared/utilities/formatCurrency';
 
 import type { TransactionPaymentCategoryProps } from '@/types/TransactionPayment';
+import { useTranslations } from 'next-intl';
 
 type CategoryCardProps = {
   category: TransactionPaymentCategoryProps;
@@ -16,45 +17,56 @@ const CategoryCard = ({
   category,
   currency,
   handleCardClick
-}: CategoryCardProps) => (
-  <Card
-    onClick={() => handleCardClick(category)}
-    className="bg-gray-100 dark:bg-gray-900 cursor-pointer"
-  >
-    <CardHeader>
-      {/* <CardDescription>{category.name}</CardDescription> */}
-      <div className="flex flex-row items-center justify-between gap-4">
-        <p className="text-sm text-slate-500 dark:text-slate-400 truncate hover:text-clip">
-          {category.name}
-        </p>
+}: CategoryCardProps) => {
+  const t = useTranslations('Page.dashboard.card');
 
-        {<CardIcon icon={category.icon} />}
-      </div>
-      <h1 className="text-lg font-bold sm:text-xl sm:font-bold">
-        {formatCurrency({
-          value: category.totalAmount,
-          currency
-        })}
-      </h1>
-    </CardHeader>
-    <CardContent>
-      <p className="text-xs text-gray-500">Settled</p>
+  return (
+    <Card
+      onClick={() => handleCardClick(category)}
+      className="bg-secondary/70 cursor-pointer"
+    >
+      <CardHeader>
+        <div className="flex flex-row items-center justify-between gap-4">
+          <p className="text-sm text-muted-foreground truncate hover:text-clip">
+            {category.name}
+          </p>
 
-      <div className="flex flex-row items-center pt-2">
-        <p className="text-xs pr-2">
-          {Math.floor((category.totalPaidAmount / category.totalAmount) * 100)}%
-        </p>
-        <Progress
-          aria-label="Loading..."
-          value={(category.totalPaidAmount / category.totalAmount) * 100}
-          classNames={{
-            indicator: 'bg-primary'
-          }}
-          size="sm"
-        />
-      </div>
-    </CardContent>
-  </Card>
-);
+          {
+            <CardIcon
+              icon={category.icon}
+              className="w-5 h-5 sm:w-6 sm:h-6 text-muted-foreground"
+            />
+          }
+        </div>
+        <h1 className="text-lg font-bold sm:text-xl sm:font-bold">
+          {formatCurrency({
+            value: category.totalAmount,
+            currency
+          })}
+        </h1>
+      </CardHeader>
+      <CardContent>
+        <p className="text-xs text-muted-foreground">{t('settled')}</p>
+
+        <div className="flex flex-row items-center pt-2">
+          <p className="text-xs pr-2">
+            {Math.floor(
+              (category.totalPaidAmount / category.totalAmount) * 100
+            )}
+            %
+          </p>
+          <Progress
+            aria-label="Loading..."
+            value={(category.totalPaidAmount / category.totalAmount) * 100}
+            classNames={{
+              indicator: 'bg-primary'
+            }}
+            size="sm"
+          />
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default CategoryCard;
