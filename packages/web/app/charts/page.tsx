e13aa50159b8b584c0125a5e0ec2adfb95bb5fd5
Darwin Jason Pajares <xsgdp001@singlife.com>
2024-11-26 16:57:39 +0800

    chore: remove mocked data

--- packages/web/app/charts/page.tsx
+++ packages/web/app/charts/page.tsx
@@ -23,15 +23,6 @@ import moment from 'moment';
 import { useEffect, useState } from 'react';
 import { Area, AreaChart, CartesianGrid, XAxis } from 'recharts';
 
-const chartData = [
-  { month: 'January', desktop: 186, mobile: 80 },
-  { month: 'February', desktop: 305, mobile: 200 },
-  { month: 'March', desktop: 237, mobile: 120 },
-  { month: 'April', desktop: 73, mobile: 190 },
-  { month: 'May', desktop: 209, mobile: 130 },
-  { month: 'June', desktop: 214, mobile: 140 }
-];
-
 const Charts = () => {
   const [date, setDate] = useState(new Date());
   const [chartData, setChartData] = useState([]);
