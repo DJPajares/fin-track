477e8736385a177a2e9b1cc67c3395df69f26de9
Darwin Jason Pajares <xsgdp001@singlife.com>
2024-11-26 16:42:22 +0800

    style: add charts on chart page

--- packages/web/app/charts/page.tsx
+++ packages/web/app/charts/page.tsx
@@ -2,12 +2,59 @@
 
 import { DatePicker } from '@/components/shared/DatePicker';
 import { Button } from '@/components/ui/button';
-import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react';
+import { Card } from '@/components/ui/card';
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent
+} from '@/components/ui/chart';
+import { useAppSelector } from '@/lib/hooks';
+import { fetchTransactionsDateRangeByType } from '@/providers/fetchTransactions';
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  TrendingDownIcon,
+  TrendingUpIcon
+} from 'lucide-react';
 import moment from 'moment';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Area, AreaChart, CartesianGrid, XAxis } from 'recharts';
+
+const chartData = [
+  { month: 'January', desktop: 186, mobile: 80 },
+  { month: 'February', desktop: 305, mobile: 200 },
+  { month: 'March', desktop: 237, mobile: 120 },
+  { month: 'April', desktop: 73, mobile: 190 },
+  { month: 'May', desktop: 209, mobile: 130 },
+  { month: 'June', desktop: 214, mobile: 140 }
+];
 
 const Charts = () => {
   const [date, setDate] = useState(new Date());
+  const [chartData, setChartData] = useState([]);
+
+  const { currency } = useAppSelector((state) => state.dashboard);
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [currency]);
+
+  const fetchData = async () => {
+    const transactions = await fetchTransactionsDateRangeByType({
+      startDate: new Date('2025-01-01T16:00:00.000Z'),
+      endDate: new Date('2025-12-01T16:00:00.000Z'),
+      currency: currency.name
+    });
+
+    setChartData(transactions);
+  };
 
   const handlePrevMonth = () => {
     const newDate = moment(date).add(-1, 'months');
@@ -20,6 +67,19 @@ const Charts = () => {
 
     setDate(moment(newDate).toDate());
   };
+
+  const chartConfig = {
+    expense: {
+      label: 'Expense',
+      color: 'hsl(var(--chart-primary))',
+      icon: TrendingDownIcon
+    },
+    income: {
+      label: 'Income',
+      color: 'hsl(var(--chart-secondary))',
+      icon: TrendingUpIcon
+    }
+  } satisfies ChartConfig;
 
   return (
     <div className="space-y-4 sm:space-y-8">
@@ -39,6 +99,52 @@ const Charts = () => {
         <Button variant="ghost" size="icon" onClick={handleNextMonth}>
           <ChevronRightIcon className="h-4 w-4" />
         </Button>
+      </div>
+
+      {/* CHART */}
+      <div>
+        <Card className="py-3 px-1">
+          <ChartContainer config={chartConfig}>
+            <AreaChart
+              accessibilityLayer
+              data={chartData}
+              margin={{
+                left: 12,
+                right: 12
+              }}
+            >
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="month"
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                tickFormatter={(value) => value.slice(0, 3)}
+              />
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent indicator="line" />}
+              />
+              <Area
+                dataKey="expense"
+                type="natural"
+                fill="var(--color-expense)"
+                // fillOpacity={0.4}
+                stroke="var(--color-expense)"
+                stackId="a"
+              />
+              <Area
+                dataKey="income"
+                type="natural"
+                fill="var(--color-income)"
+                // fillOpacity={0.4}
+                stroke="var(--color-income)"
+                stackId="a"
+              />
+              <ChartLegend content={<ChartLegendContent />} />
+            </AreaChart>
+          </ChartContainer>
+        </Card>
       </div>
     </div>
   );
