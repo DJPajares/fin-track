a5af4455a34122d8adf62d51c77e3a72fab11c02
DJPajares <dj.pajares@gmail.com>
2024-11-27 21:02:44 +0800

    refactor: update imports of shadcn components

--- packages/web/components/ui/calendar.tsx
+++ packages/web/components/ui/calendar.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import * as React from 'react';
-import { Button, buttonVariants } from '@/components/ui/button';
-import { cn } from '@/lib/utils';
+import { Button, buttonVariants } from '@web/components/ui/button';
+import { cn } from '@web/lib/utils';
 import { differenceInCalendarDays } from 'date-fns';
 import {
   DayPicker,
--- packages/web/components/ui/sidebar.tsx
+++ packages/web/components/ui/sidebar.tsx
@@ -6,19 +6,19 @@ import { VariantProps, cva } from 'class-variance-authority';
 // import { PanelLeft } from 'lucide-react';
 import { AlignLeftIcon } from 'lucide-react';
 
-import { useIsMobile } from '@/hooks/use-mobile';
-import { cn } from '@/lib/utils';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Separator } from '@/components/ui/separator';
-import { Sheet, SheetContent, SheetTitle } from '@/components/ui/sheet';
-import { Skeleton } from '@/components/ui/skeleton';
+import { useIsMobile } from '@web/hooks/use-mobile';
+import { cn } from '@web/lib/utils';
+import { Button } from '@web/components/ui/button';
+import { Input } from '@web/components/ui/input';
+import { Separator } from '@web/components/ui/separator';
+import { Sheet, SheetContent, SheetTitle } from '@web/components/ui/sheet';
+import { Skeleton } from '@web/components/ui/skeleton';
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger
-} from '@/components/ui/tooltip';
+} from '@web/components/ui/tooltip';
 
 const SIDEBAR_COOKIE_NAME = 'sidebar:state';
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
