dd5070a8718b7a05be471073a6a4f6776bbe526b
Darwin Jason Pajares <xsgdp001@singlife.com>
2024-12-17 16:30:59 +0800

    feat: round ceil on formatCurrency

--- shared/utilities/formatCurrency.ts
+++ shared/utilities/formatCurrency.ts
@@ -1,5 +1,38 @@
 import currencyLocaleMapping from '../constants/currencyLocaleMapping';
 
+type ExtendedNumberFormatOptions = Intl.NumberFormatOptions & {
+  roundingPriority?: 'auto' | 'morePrecision' | 'lessPrecision' | undefined;
+  roundingIncrement?:
+    | 1
+    | 2
+    | 5
+    | 10
+    | 20
+    | 25
+    | 50
+    | 100
+    | 200
+    | 250
+    | 500
+    | 1000
+    | 2000
+    | 2500
+    | 5000
+    | undefined;
+  roundingMode?:
+    | 'ceil'
+    | 'floor'
+    | 'expand'
+    | 'trunc'
+    | 'halfCeil'
+    | 'halfFloor'
+    | 'halfExpand'
+    | 'halfTrunc'
+    | 'halfEven'
+    | undefined;
+  trailingZeroDisplay?: 'auto' | 'stripIfInteger' | undefined;
+};
+
 type FormatCurrencyProps = {
   value: number;
   currency: string;
@@ -14,10 +47,13 @@ export const formatCurrency = ({
   const locale =
     currencyLocaleMapping[currency as keyof typeof currencyLocaleMapping];
 
-  return new Intl.NumberFormat(locale, {
+  const options: ExtendedNumberFormatOptions = {
     style: 'currency',
     currency,
     minimumFractionDigits: 0,
-    maximumFractionDigits: decimal
-  }).format(value);
+    maximumFractionDigits: decimal,
+    roundingMode: 'ceil'
+  };
+
+  return new Intl.NumberFormat(locale, options).format(value);
 };
