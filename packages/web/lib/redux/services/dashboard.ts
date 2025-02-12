f689b003485ad571499f8e7d9c73a5d87b9a63ee
Darwin Jason Pajares <xsgdp001@singlife.com>
2025-02-12 17:29:59 +0800

    refactor: use query instead of queryfn in dashboard create api

--- packages/web/lib/redux/services/dashboard.ts
+++ packages/web/lib/redux/services/dashboard.ts
@@ -1,66 +1,53 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import transactionPaymentsData from '@shared/mockData/transactionPayments.json';
-import formatDashboardQueryKey from '@shared/utilities/formatDashboardQueryKey';
+import formatYearMonth from '@shared/utilities/formatYearMonth';
 
 const useMockedData = process.env.NEXT_PUBLIC_USE_MOCKED_DATA === 'true';
 
-const transactionPaymentsUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/transactionPayments`;
+const type = 'Dashboard';
+
+type FormatQueryKeyProps = {
+  date: Date;
+  currency: string;
+};
+
+const formatQueryKey = ({ date, currency }: FormatQueryKeyProps) => {
+  const yearMonth = formatYearMonth(new Date(date));
+
+  return `${yearMonth}_${currency}`;
+};
 
 export const dashboardApi = createApi({
   reducerPath: 'dashboardApi',
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_BASE_URL }),
-  tagTypes: ['Dashboard'],
+  tagTypes: [type],
   endpoints: (builder) => ({
     getDashboardData: builder.query({
-      queryFn: async ({ date, currency }) => {
-        try {
-          if (useMockedData) {
-            return { data: transactionPaymentsData };
-          } else {
-            const response = await fetch(transactionPaymentsUrl, {
+      query: ({ date, currency }) => {
+        return useMockedData
+          ? { url: '', method: 'GET' } // Empty request since data is mocked
+          : {
+              url: '/transactionPayments',
               method: 'POST',
               headers: { 'Content-Type': 'application/json' },
-              body: JSON.stringify({ date, currency }),
-            });
-
-            if (!response.ok) {
-              throw new Error('API request failed');
-            }
-
-            const json = await response.json();
-            return { data: json.data };
-          }
-        } catch (error) {
-          let errorMessage = 'Failed to fetch dashboard data';
-          const errorStatus = 500;
-
-          if (error instanceof Error) {
-            errorMessage = error.message;
-          }
-
-          console.error('Fetch failed:', errorMessage);
-
-          return {
-            error: {
-              status: errorStatus,
-              statusText: errorMessage,
-              data: errorMessage,
-            },
-          };
-        }
+              body: { date, currency },
+            };
+      },
+      transformResponse: (response: Record<string, unknown>) => {
+        return useMockedData ? transactionPaymentsData : response.data;
       },
       providesTags: (result, error, { date, currency }) => {
         return [
           {
-            type: 'Dashboard',
-            id: formatDashboardQueryKey({ date, currency }),
+            type: type,
+            id: formatQueryKey({ date, currency }),
           },
         ];
       },
       serializeQueryArgs: ({ endpointName, queryArgs }) => {
         const { date, currency } = queryArgs;
 
-        const provider = formatDashboardQueryKey({ date, currency });
+        const provider = formatQueryKey({ date, currency });
 
         return `${endpointName}_${provider}`;
       },
@@ -72,7 +59,7 @@ export const dashboardApi = createApi({
         body: updatedData,
       }),
       invalidatesTags: (_result, _error, { date, currency }) => [
-        { type: 'Dashboard', id: formatDashboardQueryKey({ date, currency }) },
+        { type: type, id: formatQueryKey({ date, currency }) },
       ],
     }),
     updateDashboardPayments: builder.mutation({
@@ -84,7 +71,7 @@ export const dashboardApi = createApi({
       async onQueryStarted(_, { dispatch, queryFulfilled }) {
         try {
           await queryFulfilled;
-          dispatch(dashboardApi.util.invalidateTags(['Dashboard']));
+          dispatch(dashboardApi.util.invalidateTags([type]));
         } catch (error) {
           console.error('Update failed:', error);
         }
