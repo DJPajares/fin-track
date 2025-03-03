316b497d2e6ad6818189a27dd4fb16502491bc10
Darwin Jason Pajares <dj.pajares@gmail.com>
2025-03-03 21:07:49 +0800

    fix: api issue due to changing output type of formatYearMonth

--- shared/utilities/formatYearMonth.ts
+++ shared/utilities/formatYearMonth.ts
@@ -2,7 +2,7 @@ const formatYearMonth = (date: Date) => {
   const year = new Date(date).getFullYear();
   const month = new Date(date).getMonth() + 1;
   const paddedMonth = month.toString().padStart(2, '0');
-  const yearMonth = `${year}${paddedMonth}`;
+  const yearMonth = parseInt(`${year}${paddedMonth}`);
 
   return yearMonth;
 };
