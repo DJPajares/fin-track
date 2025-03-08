f5ff3c516466d562669d8542574bf5e7ef25c239
DJPajares <dj.pajares@gmail.com>
2025-03-09 00:13:59 +0800

    style: change app font to raleway

--- packages/web/app/layout.tsx
+++ packages/web/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-
+import { Raleway } from 'next/font/google';
 import './globals.css';
 import { Providers } from '../providers/providers';
 import NavBar from '../components/Nav/NavBar';
@@ -7,13 +7,19 @@ import { getLocale, getMessages } from 'next-intl/server';
 import { NextIntlClientProvider } from 'next-intl';
 import { Toaster } from '../components/ui/toaster';
 
+const raleway = Raleway({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-raleway',
+});
+
 export const metadata: Metadata = {
   title: 'Fin-Track',
-  description: 'Financial tracker tool'
+  description: 'Financial tracker tool',
 };
 
 export default async function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
@@ -21,19 +27,23 @@ export default async function RootLayout({
   const messages = await getMessages();
 
   return (
-    <html lang={locale} suppressHydrationWarning>
+    <html
+      lang={locale}
+      suppressHydrationWarning
+      className={`${raleway.variable}`}
+    >
       <head>
         <meta
           name="viewport"
           content="width=device-width, height=device-height, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"
         />
       </head>
-      <body className="antialiased">
+      <body className={`antialiased ${raleway.className}`}>
         <NextIntlClientProvider messages={messages}>
           <Providers>
             <main className="bg-white dark:bg-slate-950" vaul-drawer-wrapper="">
               <NavBar>
-                <div className="w-full py-2 px-6 sm:px-8">{children}</div>
+                <div className="w-full px-6 py-2 sm:px-8">{children}</div>
               </NavBar>
             </main>
 
