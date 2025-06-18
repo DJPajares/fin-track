84da0a7f8a52f1bb9a9a03aeb56146dea31053ac
Darwin Jason Pajares <xsgdp001@singlife.com>
2025-06-18 14:04:09 +0800

    chore: remove dev indicators

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from 'next';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
+  devIndicators: false,
   // reactStrictMode: true,
   // images: {
   //   unoptimized: true
