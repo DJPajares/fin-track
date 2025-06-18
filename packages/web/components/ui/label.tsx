6b0e1869fb36d0a9efd8a6ff8f52cbd3dde045c7
Darwin Jason Pajares <xsgdp001@singlife.com>
2025-06-18 10:50:49 +0800

    feat: add label error variant

--- packages/web/components/ui/label.tsx
+++ packages/web/components/ui/label.tsx
@@ -15,8 +15,10 @@ const labelVariants = cva(
         title: 'font-semibold text-lg sm:text-xl sm:font-bold',
         title_lg: 'font-bold text-2xl sm:text-3xl sm:font-extrabold',
         title_md: 'font-medium text-lg sm:text-xl sm:font-semibold',
+        title_sm: 'font-medium text-base sm:text-lg sm:font-semibold',
         subtitle: 'text-base font-extralight sm:text-lg sm:font-light',
         caption: 'text-xs font-light sm:text-sm sm:font-normal',
+        error: 'text-destructive font-semibold',
       },
     },
     defaultVariants: {
