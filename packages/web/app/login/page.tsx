8384aa1be8ec6a2736a3c3f63478111bc358b5cf
Darwin Jason Pajares <xsgdp001@singlife.com>
2025-07-01 14:19:22 +0800

    style: use shadcn login form block

--- packages/web/app/login/page.tsx
+++ packages/web/app/login/page.tsx
@@ -1,31 +1,12 @@
 'use client';
 
-import { signIn } from 'next-auth/react';
-import { GithubIcon } from 'lucide-react';
+import { LoginForm } from '@web/components/Login/login-form';
 
-import { Button } from '@web/components/ui/button';
-import { Label } from '@web/components/ui/label';
-
-export default function SignIn() {
+export default function Login() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="w-full max-w-md space-y-6 p-8">
-        <div className="flex flex-col items-center space-y-2 text-center">
-          <Label variant="title-lg">Fin-Track</Label>
-          <Label variant="subtitle-md" className="text-muted-foreground">
-            Sign in to your account
-          </Label>
-        </div>
-
-        <div className="space-y-4">
-          <Button
-            className="w-full bg-neutral-900 text-white hover:bg-neutral-800"
-            onClick={() => signIn('github', { redirectTo: '/' })}
-          >
-            <GithubIcon className="mr-2 h-5 w-5" />
-            Sign in with GitHub
-          </Button>
-        </div>
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <LoginForm />
       </div>
     </div>
   );
