2b847cafbb4815478ae27b4214ece014376780a8
Darwin Jason Pajares <xsgdp001@singlife.com>
2025-12-19 16:13:40 +0800

    chore: move translations from public to messages as next-intl recommends

--- packages/web/i18n/request.ts
+++ packages/web/i18n/request.ts
@@ -2,13 +2,10 @@ import { getUserLocale } from '../services/locale';
 import { getRequestConfig } from 'next-intl/server';
 
 export default getRequestConfig(async () => {
-  // Provide a static locale, fetch a user setting,
-  // read from `cookies()`, `headers()`, etc.
-  // const locale = 'en';
   const locale = await getUserLocale();
 
   return {
     locale,
-    messages: (await import(`../public/locales/${locale}.json`)).default
+    messages: (await import(`../messages/${locale}.json`)).default,
   };
 });
