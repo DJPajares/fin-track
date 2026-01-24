9a862a7a820014b303438491e0e373e0603a6d5a
DJPajares <dj.pajares@gmail.com>
2026-01-24 21:42:51 +0800

    fix: onboarding page not full height

--- packages/web/app/onboarding/layout.tsx
+++ packages/web/app/onboarding/layout.tsx
@@ -4,7 +4,7 @@ export default function OnboardingLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="bg-background flex min-h-[calc(100dvh-3rem)] items-center justify-center sm:min-h-[calc(100dvh-3.5rem)]">
+    <main className="bg-background flex min-h-[calc(100dvh-2rem)] items-center justify-center sm:min-h-[calc(100dvh-3.5rem)]">
       <div className="w-full max-w-md">{children}</div>
     </main>
   );
